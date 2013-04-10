48eb72ac6cc5a2504783910c9181f7bb1cc54dd0
Jeff Burnett <jeffrey.burnett31@gmail.com>
2013-04-10 14:53:15 -0500

    converter is working

--- encode/src/encode/core.clj
+++ encode/src/encode/core.clj
@@ -1,17 +1,27 @@
 (ns encode.core
   (import [org.apache.commons.lang StringEscapeUtils])
-  (require [seesaw.core :refer :all]))
+  (require [seesaw.core :refer :all]
+           [seesaw.chooser :refer (choose-file)]
+           [clojure.java.io :refer :all]))
 
-(defn create-frame []
+(defn encodeframe []
   (native!)
   (let [f (frame :title "Convert database" :on-close :exit)
         lbl (label "Database file")
         chfile (button :text "Browse")
         txt (text :columns 30)
-        pan (flow-panel :items [lbl txt chfile])]
+        go (button :text "Convert")
+        pan (flow-panel :items [lbl txt chfile go])]
     (config! f :content pan)
-    (-> f pack! show!)))
+    (-> f pack! show!)
+    (listen chfile :action (fn [e]
+                             (if-let [f (choose-file)]
+                               (text! txt (.getAbsolutePath f)))))
+    (listen go :action (fn [e]
+                         (let [in (slurp (file (text txt)))
+                               out (file "residents.js")]
+                           (spit out (str "jsObj = \"" (StringEscapeUtils/escapeJavaScript in) "\"")))))))
 
 (defn -main []
-  (create-frame)
+  (encodeframe)
   (println (StringEscapeUtils/escapeJavaScript "\"hello, world\"")))
